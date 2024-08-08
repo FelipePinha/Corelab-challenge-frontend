6c17fdd635b68b626cf8d5e772c463e7a21e40ed
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-08-08 10:47:15 -0300

    create header layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,20 @@
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <header className="h-14 flex items-center justify-between px-6 bg-white shadow-sm">
+        <div className="flex items-center gap-5 w-[80%]">
+          <img src="logo.svg" />
+          <input type="text" className="border-2 px-2 h-8 rounded-md text-sm w-full outline-none" placeholder="Pesquisar notas"/>
+        </div>
+
+        <div>
+          <img src="x.svg" />
+        </div>
+      </header>
+
+      <main>
+
+      </main>
+    </div>
   )
 }
