d0590027a8141da4632d9761f9b3d2e12abb751e
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-08-08 11:28:03 -0300

    create todo button

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <div>
+    <div className="space-y-6">
       <header className="h-14 flex items-center justify-between px-6 bg-white shadow-sm">
         <div className="flex items-center gap-5 w-[80%]">
           <img src="logo.svg" />
@@ -12,8 +12,24 @@ export function App() {
         </div>
       </header>
 
-      <main>
+      <main className="max-w-7xl flex justify-center">
+          <section className="w-full flex justify-center">
+            <button className="w-full flex justify-center">
+              <div className="flex flex-col rounded-lg w-[60%] p-5 shadow-sm bg-white space-y-3">
+                <div className="flex item-center justify-between w-full">
+                  <h3 className="font-bold">Título</h3>
 
+                  <img src="star.svg" />
+                </div>
+
+                <div className="border-b border-zinc-300"/>
+
+                <div className="text-left">
+                  <p>Criar nota...</p>
+                </div>
+              </div>
+            </button>
+          </section>
       </main>
     </div>
   )
